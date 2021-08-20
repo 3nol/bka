da31de11f80ede67733e4c88d53a0a375c5c123c
Leon Wenzler <leon.wenzler@gmail.com>
2021-08-20 23:43:35 +0200

    fixing wrong comma placements and using attributes getters

--- BKA.py
+++ BKA.py
@@ -1,3 +1,4 @@
+import bs4.element
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -17,26 +18,50 @@ class BKA:
     def get_wanted_persons(self, depth: int):
         """Retrieves json wanted list, parameter depth determines how often more entries should be loaded"""
 
+        wanted_persons_list = ''
+
         for i in range(depth):
             soup = BeautifulSoup(requests.get(self.site + self.list + str(i + 1)).text, 'html.parser')
-            for person in soup.find_all('div', class_='slide teaser type-2'):
-                img = self.site + person.find('img').get('src').split(';', 1)[0] + '?__blob=normal'
-                category = str(person.find('span', class_='category').text).strip()
-                name = str(person.find('h3').text).strip().replace('\u00ad', '')
-                info = ''
-                for fact in str(person.find('p').text).strip().split('\n'):
-                    if fact != '':
-                        (key, value) = fact.split(':', 1)
-                        info += f'"{key}": "{value}",\n'
-
-                json_string = '{\n' + indent_units(
-                    f'"category": "{category}",\n'
-                    f'"img": "{img}",\n'
-                    f'"name": "{name}",\n'
+            persons = soup.find_all('div', class_='slide teaser type-2')
+            for p in range(len(persons)):
+                wanted_persons_list += '{\n' + indent_units(
+                    f'"category": "{self.__get_category(persons[p])}",\n'
+                    f'"img": "{self.__get_img(persons[p])}",\n'
+                    f'"name": "{self.__get_name(persons[p])}",\n'
                     '"info": {\n'
-                    f'{indent_units(info, 2)}\n'
-                    '}', 2) + '\n},'
+                    f'{indent_units(self.__get_info(persons[p]), 2)}\n'
+                    '}', 2) + '\n}'
+                if i < depth - 1 or p < len(persons) - 1:
+                    wanted_persons_list += ','
+        wanted_persons_list = '[\n' + indent_units(wanted_persons_list, 2) + '\n]'
+        print(wanted_persons_list)
+        return json.loads(wanted_persons_list)
 
-                print(indent_units(json_string, 2))
-                return json.loads(json_string)
+    def __get_img(self, person: bs4.element.Tag):
+        img_container = person.find('img')
+        if img_container is not None:
+            return self.site + img_container.get('src').split(';', 1)[0] + '?__blob=normal'
+        return ''
 
+    def __get_category(self, person: bs4.element.Tag):
+        category_container = person.find('span', class_='category')
+        if category_container is not None:
+            return str(category_container.text).strip()
+        return ''
+
+    def __get_name(self, person: bs4.element.Tag):
+        name_container = person.find('h3')
+        if name_container is not None:
+            return str(name_container.text).strip().replace('\u00ad', '')
+        return ''
+
+    def __get_info(self, person: bs4.element.Tag):
+        info_container = person.find('p')
+        if info_container is not None:
+            info = ''
+            for fact in str(info_container.text).strip().split('\n'):
+                if fact != '':
+                    (key, value) = fact.split(':', 1)
+                    info += f'"{key}": "{value}",\n'
+            return info.rsplit(',\n', 1)[0]
+        return ''
