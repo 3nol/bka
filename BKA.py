92ba1f176c9c3ba72417ebf3ded30bc7418743e8
Leon Wenzler <leon.wenzler@gmail.com>
2021-08-22 10:05:57 +0200

    removing parameter depth, closes issue #1, adding pretty_print option

--- BKA.py
+++ BKA.py
@@ -1,6 +1,5 @@
-import bs4.element
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 import json
 
 
@@ -18,48 +17,52 @@ class BKA:
         # appendix for the wanted list
         self.list = '/SiteGlobals/Forms/Suche/Fahndungsliste_Personenfahndung_Formular.html?nn=4210&gtp=3922_list%253D'
 
-    def get_wanted_persons(self, depth: int):
+    def get_wanted_persons(self, pretty_print: bool = False) -> list[dict]:
         """Retrieves json wanted list, parameter depth determines how often more entries should be loaded"""
         wanted_persons_list = ''
-        for i in range(depth):
+        i = 1
+        while True:
             soup = BeautifulSoup(requests.get(self.site + self.list + str(i + 1)).text, 'html.parser')
             persons = soup.find_all('div', class_='slide teaser type-2')
-            for p in range(len(persons)):
-                wanted_persons_list += '{\n' + indent_units(
-                    f'"category": "{self.__get_category(persons[p])}",\n'
-                    f'"img": "{self.__get_img(persons[p])}",\n'
-                    f'"name": "{self.__get_name(persons[p])}",\n'
-                    '"info": {\n'
-                    f'{indent_units(self.__get_info(persons[p]), 2)}\n'
-                    '}', 2) + '\n}'
-                if i < depth - 1 or p < len(persons) - 1:
-                    wanted_persons_list += ','
-        wanted_persons_list = '[\n' + indent_units(wanted_persons_list, 2) + '\n]'
-        print(wanted_persons_list)
+            if persons:
+                for p in range(len(persons)):
+                    wanted_persons_list += '{\n' + indent_units(
+                        f'"category": "{self.__get_category(persons[p])}",\n'
+                        f'"img": "{self.__get_img(persons[p])}",\n'
+                        f'"name": "{self.__get_name(persons[p])}",\n'
+                        '"info": {\n'
+                        f'{indent_units(self.__get_info(persons[p]), 2)}\n'
+                        '}', 2) + '\n},'
+                i += 1
+            else:
+                break
+        wanted_persons_list = '[\n' + indent_units(wanted_persons_list[0:-1], 2) + '\n]'
+        if pretty_print:
+            print(wanted_persons_list)
         return json.loads(wanted_persons_list)
 
-    def __get_img(self, person: bs4.element.Tag):
+    def __get_img(self, person: element.Tag) -> str:
         """Retrieves the image url"""
         img_container = person.find('img')
         if img_container is not None:
             return self.site + img_container.get('src').split(';', 1)[0] + '?__blob=normal'
         return ''
 
-    def __get_category(self, person: bs4.element.Tag):
+    def __get_category(self, person: element.Tag) -> str:
         """Retrieves the entry category, e.g. 'Gesucht'"""
         category_container = person.find('span', class_='category')
         if category_container is not None:
             return str(category_container.text).strip()
         return ''
 
-    def __get_name(self, person: bs4.element.Tag):
+    def __get_name(self, person: element.Tag) -> str:
         """Retrieves the person's name (sometimes unknown)"""
         name_container = person.find('h3')
         if name_container is not None:
             return str(name_container.text).strip().replace('\u00ad', '')
         return ''
 
-    def __get_info(self, person: bs4.element.Tag):
+    def __get_info(self, person: element.Tag) -> str:
         """Collects all further information listed on the site, e.g. 'Delikt', 'Tatort', ..."""
         info_container = person.find('p')
         if info_container is not None:
